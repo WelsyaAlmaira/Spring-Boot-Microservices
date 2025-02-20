56ada5d096ef5253b96035bf571254d8a41512a6
WelsyaAlmaira <welsyaalmaira@gmail.com>
2025-02-21 04:04:08 +0700

    part8: aggregate rest api in gateway

--- api-gateway/src/main/java/com/programming/techie/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/programming/techie/gateway/config/SecurityConfig.java
@@ -9,9 +9,13 @@ import org.springframework.security.config.Customizer;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls).permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
